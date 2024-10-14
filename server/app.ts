3f155853437f9b51e3d31cc60319642e53ed02e2
shubham-200315 <sj428731@gmail.com>
2024-10-14 21:00:34 +0530

    server setup

--- server/app.ts
+++ server/app.ts
@@ -1,2 +1,19 @@
+require("dotenv").config();
 import express from 'express'
 export const app = express()
+import cors from "cors"
+import cookieParser from "cookie-parser";
+
+// body parser
+app.use(express.json({ limit: "50mb" }));
+
+// cookie parser
+app.use(cookieParser());
+
+// cors => cross origin resource sharing
+// origin: process.env.ORIGIN,
+app.use(
+    cors({
+        origin: process.env.ORIGIN,
+    })
+);
