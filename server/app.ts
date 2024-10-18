df5e382dbd0331215424f3c4f1c5a51856abdf5e
shubham-200315 <sj428731@gmail.com>
2024-10-18 09:57:11 +0530

    Added protected route

--- server/app.ts
+++ server/app.ts
@@ -1,5 +1,5 @@
 require("dotenv").config();
-import express from 'express'
+import express, { NextFunction, Request, Response } from 'express'
 export const app = express()
 import cors from "cors"
 import cookieParser from "cookie-parser";
@@ -11,9 +11,23 @@ app.use(express.json({ limit: "50mb" }));
 app.use(cookieParser());
 
 // cors => cross origin resource sharing
-// origin: process.env.ORIGIN,
 app.use(
     cors({
         origin: process.env.ORIGIN,
     })
 );
+
+// testing api
+app.get("/test", (req: Request, res: Response, next: NextFunction) => {
+    res.status(200).json({
+        succcess: true,
+        message: "API is working",
+    });
+});
+
+// unknown route
+app.all("*", (req: Request, res: Response, next: NextFunction) => {
+    const err = new Error(`Route ${req.originalUrl} not found`) as any;
+    err.statusCode = 404;
+    next(err);
+});
