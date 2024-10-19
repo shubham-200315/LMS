b9f5c7762ad340e62cdc509cac8f6bc1f36e159b
shubham-200315 <sj428731@gmail.com>
2024-10-19 17:03:37 +0530

    Database connection

--- server/server.ts
+++ server/server.ts
@@ -1,3 +1,4 @@
+import connectDB from './utils/db';
 import {app} from './app';
 require('dotenv').config();
 
@@ -5,4 +6,5 @@ require('dotenv').config();
 //create server
 app.listen(process.env.PORT, () =>{
     console.log(`Server is running on port ${process.env.PORT}`)
+    connectDB();
 })
